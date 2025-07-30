0dc3e9825f7f3447611c02d20ca8f6c7d6fad16d
Cameron Brown <moontsukuyomi6@gmail.com>
2025-07-30 16:59:11 +0200

    Add: add a class to handle creating a list object and grouping all related functions

---
+++ javascript.js
@@ -0,0 +1,15 @@
+class LinkedList {
+  constructor(list = {}) {
+    this.list = list;
+  }
+  append(value) {}
+  prepend(value) {}
+  size() {}
+  head() {}
+  tail() {}
+  at(index) {}
+  pop() {}
+  contains(value) {}
+  find(value) {}
+  toString() {}
+}
